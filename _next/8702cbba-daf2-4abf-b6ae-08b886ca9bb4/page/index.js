5fd423101706662948f01f308f2fc038df7949b2
Elena Pavel <t.twixx7@gmail.com>
2018-07-01 21:58:28 +0300

    compiled files.

--- _next/8702cbba-daf2-4abf-b6ae-08b886ca9bb4/page/index.js
+++
@@ -1,1 +0,0 @@
-module.exports=__NEXT_REGISTER_PAGE("/",function(){return{page:webpackJsonp([2],{195:function(e,t,n){e.exports=n(196)},196:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var l=n(12),o=n.n(l);t.default=o.a.createElement("b",null,"Hello world")}},[195]).default}});
