c5a2234d53ca38ecc5cdaa91e88617e71f3674fe
Elena Pavel <t.twixx7@gmail.com>
2018-07-01 22:06:25 +0300

    compiled files.

--- .next/server/bundles/pages/_app.js
+++ .next/server/bundles/pages/_app.js
@@ -67,28 +67,28 @@ module.exports =
 /******/ 	__webpack_require__.p = "/navaru";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 6);
+/******/ 	return __webpack_require__(__webpack_require__.s = 7);
 /******/ })
 /************************************************************************/
 /******/ ({
 
-/***/ 6:
-/***/ (function(module, exports, __webpack_require__) {
-
-module.exports = __webpack_require__(7);
-
-
-/***/ }),
-
 /***/ 7:
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(8)
+module.exports = __webpack_require__(8);
 
 
 /***/ }),
 
 /***/ 8:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(9)
+
+
+/***/ }),
+
+/***/ 9:
 /***/ (function(module, exports) {
 
 module.exports = require("next/app");
--- .next/server/bundles/pages/index.js
+++ .next/server/bundles/pages/index.js
@@ -94,10 +94,132 @@ module.exports = __webpack_require__(5);
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_react__ = __webpack_require__(0);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_react___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_react__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__style_css__ = __webpack_require__(6);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__style_css___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1__style_css__);
 
-/* harmony default export */ __webpack_exports__["default"] = (function () {
-  return __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement("b", null, "Hello world");
-});
+
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+
+
+
+var Page =
+/*#__PURE__*/
+function (_Component) {
+  _inherits(Page, _Component);
+
+  function Page() {
+    var _ref;
+
+    var _temp, _this;
+
+    _classCallCheck(this, Page);
+
+    for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
+      args[_key] = arguments[_key];
+    }
+
+    return _possibleConstructorReturn(_this, (_temp = _this = _possibleConstructorReturn(this, (_ref = Page.__proto__ || Object.getPrototypeOf(Page)).call.apply(_ref, [this].concat(args))), Object.defineProperty(_assertThisInitialized(_this), "state", {
+      configurable: true,
+      enumerable: true,
+      writable: true,
+      value: {
+        angle: 0,
+        width: 0,
+        height: 0
+      }
+    }), _temp));
+  }
+
+  _createClass(Page, [{
+    key: "componentDidMount",
+    value: function componentDidMount() {
+      var _this2 = this;
+
+      this.calcAngle();
+      window.addEventListener("resize", function () {
+        return _this2.calcAngle();
+      });
+    }
+  }, {
+    key: "calcAngle",
+    value: function calcAngle() {
+      var width = window.innerWidth;
+      var height = window.innerHeight;
+      var angle = Math.atan(height / width); // in radians
+
+      this.setState({
+        angle: angle,
+        width: width,
+        height: height
+      });
+    }
+  }, {
+    key: "clickLeft",
+    value: function clickLeft() {// console.log("left");
+    }
+  }, {
+    key: "clickRight",
+    value: function clickRight() {// console.log("right");
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      var _this3 = this;
+
+      var _state = this.state,
+          angle = _state.angle,
+          width = _state.width,
+          height = _state.height;
+      return __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement("b", {
+        className: __WEBPACK_IMPORTED_MODULE_1__style_css___default.a.layout
+      }, __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement("b", {
+        className: __WEBPACK_IMPORTED_MODULE_1__style_css___default.a.left,
+        style: {
+          transform: "rotate(".concat(angle, "rad)")
+        },
+        onClick: function onClick() {
+          return _this3.clickLeft();
+        }
+      }), __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement("b", {
+        className: __WEBPACK_IMPORTED_MODULE_1__style_css___default.a.right,
+        style: {
+          transform: "rotate(".concat(angle, "rad)")
+        },
+        onClick: function onClick() {
+          return _this3.clickRight();
+        }
+      }));
+    }
+  }]);
+
+  return Page;
+}(__WEBPACK_IMPORTED_MODULE_0_react__["Component"]);
+
+/* harmony default export */ __webpack_exports__["default"] = (Page);
+
+/***/ }),
+/* 6 */
+/***/ (function(module, exports) {
+
+module.exports = {
+	"layout": "layout_1bhwo",
+	"container": "container_37FNP",
+	"left": "left_2kYut container_37FNP",
+	"right": "right_2kU9g container_37FNP"
+};
 
 /***/ })
 /******/ ]);
