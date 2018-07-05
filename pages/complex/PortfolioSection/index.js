bfdfa43ea0e76cf5bf1aa971fc80efce6f3cb18a
Alex Gherghisan <axeminator@gmail.com>
2018-07-05 03:44:05 +0300

    clients section, portfolio, web sec, frontend sec , responsive fixes all resolutions

---
+++ pages/complex/PortfolioSection/index.js
@@ -0,0 +1,12 @@
+import { Component } from "react";
+import $ from "./style.css";
+
+export default () => (
+	<b className={$.container}>
+		<b className={$.heading}>Portfolio</b>
+		<b className={$.separator} />
+		<b className={$.items}>
+			<b className={$.item}> </b>
+		</b>
+	</b>
+);
